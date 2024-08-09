5a4f9a69fb7cbdbf3b2a009719a880648918232e
kalyan.raparthi <kalyan.raparthi@hotmain.com>
2024-08-09 11:11:16 +0530

    intial-commit

---
+++ java/Ternary.java
@@ -0,0 +1,6 @@
+class Ternary {
+    public static void main() {
+        int x = 10;
+        
+    }     
+}
---
+++ java/scope.java
@@ -0,0 +1,16 @@
+class Ternary {
+    public static void main() {
+        int x = 10;
+        if (x == 10) {
+            int y = 20;
+            int x1 = 30;
+            System.out.println(x);
+        }
+
+        System.err.println(x);
+        // System.err.println(y);
+        // error
+        // System.err.println(x1);
+        // error
+    }     
+}
