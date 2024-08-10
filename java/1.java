781e39254fd5e6ecfa716dfa992dd5320d5f9e14
qb <83409253+qb-bin@users.noreply.github.com>
2024-08-10 14:23:02 +0530

    Create 1.java

---
+++ java/1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Arithmetic {
+public static void main(String args[]) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the 'a' value: ");
+int a = scanner.nextInt();
+// System.out.println("");
+System.out.print("Enter the 'b' value: ");
+int b = scanner.nextInt();
+// System.out.println("");
+System.out.println("Addition: " + (a + b));
+System.out.println("Subtraction: " + (a - b));
+System.out.println("Multiplication: " + (a * b));
+System.out.println("Division: " + (a / b));
+System.out.println("Modulus: " + (a % b));
+  }
+}
