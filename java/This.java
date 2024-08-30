98b3c2f5546f1e8d61d0d472c4fb8e0b24cfa567
qb <kalyan.raparthi@hotmail.com>
2024-08-30 10:05:39 +0530

    This.java updated

--- java/This.java
+++ java/This.java
@@ -1,16 +1,17 @@
 class ThisObj {
-    int a;
-    ThisObj(int val) {
-        this.a = val;
+    String rno, name;
+    ThisObj(String r, String n) {
+        rno  = r;
+        name = n;
     }
     void print() {
-        System.out.println(this.a);
+        System.out.println(this.rno + " " + this.name);
     }    
 }
 
 class This {
     public static void main(String args[]) {
-        ThisObj o1 = new ThisObj(9);
+        ThisObj o1 = new ThisObj("B24DS065L", "qb");
         o1.print();
     }
 }
