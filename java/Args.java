b4d4446a62e93612064b4167ea164a6eb4db5245
qb <kalyan.raparthi@hotmail.com>
2024-08-30 11:35:43 +0530

    Args.java added

--- java/Args.java
+++ java/Args.java
@@ -1,7 +1,25 @@
 class Args {
-    public static void main(String[] args) {
-        for (String str : args) {
-            System.out.println(str);
+    public static void add(double d, int ...arr) {
+        int result = 0;
+
+        for(int i : arr) {
+            result += i;
         }
+        System.out.println(result + d);
+    }
+    public static void main(String ... args) {
+        int result = 0;
+        
+        if(args.length != 0) {
+            for(String str : args) {
+                result += Integer.parseInt(str);
+            }
+            System.out.println(result);
+        }
+
+        Args.add(9.0, 9);
+        Args.add(9);
+        Args.add(9, 9, 9, 9, 9);
+        Args.add(9.9);
     }
 }
